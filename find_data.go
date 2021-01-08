287878083a2ac85b4ce222257e03d8dc68cdb9b8
Kushuh <kuzanisu@gmail.com>
2021-01-08 17:41:51 +0100

    [DROPPED] Cbq queries

--- find_data.go
+++ find_data.go
@@ -11,33 +11,3 @@ func FindBucket(name string, buckets []*divan_types.BucketData) *divan_types.Buc
 
 	return nil
 }
-
-func FindIndex(name string, indexes []*divan_types.IndexData) *divan_types.IndexData {
-	for _, index := range indexes {
-		if index.Name == name {
-			return index
-		}
-	}
-
-	return nil
-}
-
-func FindNamespace(name string, namespaces []*divan_types.NamespaceData) *divan_types.NamespaceData {
-	for _, namespace := range namespaces {
-		if namespace.Name == name {
-			return namespace
-		}
-	}
-
-	return nil
-}
-
-func FindKeyspace(name string, keyspaces []*divan_types.KeyspaceData) *divan_types.KeyspaceData {
-	for _, keyspace := range keyspaces {
-		if keyspace.Name == name {
-			return keyspace
-		}
-	}
-
-	return nil
-}
